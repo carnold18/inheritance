c01b046c873d40dc81e6704d647a9dbdddb86245
github username <github email address>
2019-06-28 13:58:07 -0700

    added more details on lecture flow in instructions.rb

--- instructions.rb
+++ instructions.rb
@@ -1,6 +1,6 @@
 # What is inheritance?
 # Inheritance Definition
-# - handing down functionality between objects
+# - handing down functionality among objects
 # - sharing functionality
 # - relationships and hierarchy
 
@@ -12,13 +12,23 @@
 
 # Examples of Inheritance
 # - Car types
-# - Cars, models, etc.
+#   - Cars, models, etc.
+# - Animals
+#   - Cats, dogs, parrots
 
-# 1. Start with Cell Phone
-# 2. How about a landline?
+# 1. Start with CellPhone
+#   - Build a class that initializes with a method called ring
+#   - It should initialize with a type
+# 2. What other things ring? 
+#   - Create a class called Landline that also initializes with a method called ring
+#   - It should initialize with a type
 # 3. What are some commonalities between these? 
-# ----> Move methods into Module - Landline and Cell Phone inherit from Phone Module
+#   - Create a Phone Module
+#   - Move duplicate methods into Phone Module
+#   - Landline and CellPhone inherit from Phone Module
 # 4. Nested Modules for class versus instance methods
-# 5. iPhone & Android inherit from Cell Phone
-# 6. What if you dont want your kids to access your access parents methods? PRIVATE
-# 7. Move into terminal for ActiveRecord::Base.methods 
+# 5. iPhone inherits from Cell Phone
+# 6. What if you dont want your kids to access your access parents' methods? PRIVATE
+#   - All methods below the keyword 'private' can only be accessed within the designated class 
+#     even if a sub-class inherits from it
+# 7. Move into terminal for ActiveRecord::Base.methods IF this lecture is after ActiveRecord is intro'd
