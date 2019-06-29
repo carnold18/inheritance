c6f715b8cd764d3e45f9f31cd76f4f5ec00c7a4e
github username <github email address>
2019-06-29 03:23:36 -0700

    edited the md and created a lecture instructions file

--- instructions.rb
+++ instructions.rb
@@ -1,21 +1,3 @@
-## What is inheritance?
-### Inheritance Definition
-#### - handing down functionality among objects
-#### - sharing functionality
-#### - relationships and hierarchy
-
-### What are some ways you can implement inheritance in Ruby?
-#### Why is it important?
-#### Modules vs Class
-#### Private vs Protected vs Public
-#### Include vs Extend
-
-# Examples of Inheritance
-# - Car types
-#   - Cars, models, etc.
-# - Animals
-#   - Cats, dogs, parrots
-
 # 1. Start with CellPhone
 #   - Build a class that initializes with a method called ring
 #   - It should initialize with a type
